4d2556b410c47b92f7bf432a820fdb6b12ef935a
eorian <namle@namles-Mac.local>
2015-03-23 18:06:19 +0700

    add HeaderView

--- CSC M-Products/MainTableViewController.m
+++ CSC M-Products/MainTableViewController.m
@@ -10,7 +10,8 @@
 #import "MainTableViewCell.h"
 #import "JsonHelper.h"
 #import "GlobalData.h"
-@interface MainTableViewController ()<UIScrollViewDelegate>
+#import "InfinitePagingView.h"
+@interface MainTableViewController ()<UIScrollViewDelegate,InfinitePagingViewDelegate>
 @property (nonatomic, strong) NSDictionary* collectionData;
 @property (nonatomic, strong) NSMutableArray* headerArray;
 @property (nonatomic, strong) UIScrollView* headerScrollView;
@@ -19,65 +20,44 @@
 
 @implementation MainTableViewController
 {
+    InfinitePagingView* headerview;
 }
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.tableView.tag = 1000;
     self.collectionData = [self fakeData];
     self.collectionDataKeys = [NSMutableArray arrayWithArray:[[self fakeData]allKeys]];
     self.title = @"Featured";
     [GlobalData sharedManager].navigationController = self.navigationController;
     [self.tableView registerClass:[MainTableViewCell class] forCellReuseIdentifier:@"MainTableViewCell"];
     [self addHeaderView];
-    //[NSTimer scheduledTimerWithTimeInterval:3.0 target:self selector:@selector(onTimer)   userInfo:nil repeats:YES];
+    [NSTimer scheduledTimerWithTimeInterval:5.0 target:self selector:@selector(onTimer)   userInfo:nil repeats:YES];
     self.view.backgroundColor = [UIColor whiteColor];
-    
-    
+    self.automaticallyAdjustsScrollViewInsets = NO;
 
 }
 - (void)onTimer
 {
-    float h = self.headerScrollView.contentOffset.x + 320;
-    [self.headerScrollView setContentOffset:CGPointMake(h, 0) animated:YES];
-    [self scrollViewDidEndDecelerating:self.headerScrollView];
+    [headerview scrollToNextPage];
 }
 - (void)addHeaderView
 {
     JsonHelper * helper = [JsonHelper new];
-    NSArray* originalArray = [helper headerData];
-    id firstItem = [originalArray firstObject];
-    id lastItem = [originalArray lastObject];
-    self.headerArray = [NSMutableArray arrayWithArray:originalArray];
-    [self.headerArray addObject:firstItem];
-    [self.headerArray insertObject:lastItem atIndex:0];
-    self.headerScrollView = [[UIScrollView alloc]initWithFrame:CGRectMake(0, 0, self.view.bounds.size.width, 200)];
-    self.headerScrollView.tag = 1001;
-    self.headerScrollView.delegate = self;
-    self.headerScrollView.pagingEnabled = YES;
-    self.headerScrollView.showsHorizontalScrollIndicator = NO;
-    self.headerScrollView.contentSize = CGSizeMake(self.view.bounds.size.width*5, 200);
-    for (int i = 0; i < self.headerArray.count; i++) {
-        CGRect imageFrame = CGRectMake(i*320, 0, 320, 200);
-        UIImageView* header= [[UIImageView alloc]initWithFrame:imageFrame];
-        CGFloat redLevel    = rand() / (float) RAND_MAX;
-        CGFloat greenLevel  = rand() / (float) RAND_MAX;
-        CGFloat blueLevel   = rand() / (float) RAND_MAX;
-        
-        header.backgroundColor = [UIColor colorWithRed: redLevel
-                                                 green: greenLevel
-                                                  blue: blueLevel
-                                                 alpha: 1.0];
-        header.image = [UIImage imageNamed:[self.headerArray objectAtIndex:i]];
-        self.headerScrollView.contentOffset = CGPointMake(320/2 + (self.view.bounds.size.width -320)/2, 0);
-        [self.headerScrollView addSubview:header];
+    self.headerArray = [NSMutableArray arrayWithArray:[helper headerData]];
+    headerview = [[InfinitePagingView alloc]initWithFrame:CGRectMake(0, 0, self.view.bounds.size.width, 200)];
+    headerview.pageSize = CGSizeMake(320, 200);
+    headerview.delegate = self;
+    for (int i=0; i<self.headerArray.count; i++) {
+        UIImageView* imageView = [[UIImageView alloc]initWithFrame:CGRectMake(0, 0, 320, 200)];
+        imageView.image = [UIImage imageNamed:[self.headerArray objectAtIndex:i]];
+        imageView.contentMode = UIViewContentModeScaleAspectFit;
+        [headerview addPageView:imageView];
     }
+    UIView* contentView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, 320, 200)];
+    [contentView addSubview:headerview];
+    self.tableView.tableHeaderView = contentView;
+
     
-    UIView* headerSectionView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, self.view.bounds.size.width,200-66)];
-    headerSectionView.backgroundColor   = [UIColor whiteColor];
-    UIView* contentView = self.headerScrollView;
-    contentView.backgroundColor = [UIColor whiteColor];
-    [headerSectionView  addSubview:contentView];
-    self.tableView.tableHeaderView = headerSectionView;
-   
 }
 
 - (void)didReceiveMemoryWarning {
@@ -136,29 +116,10 @@
 }
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView
 {
-    //stick header
-    NSLog([NSString stringWithFormat:@"%f",scrollView.contentOffset.x]);
-    if (scrollView.tag == 1001) {
-       
-        if (scrollView.contentOffset.x == (self.headerArray.count-1)*320) {
-            scrollView.contentOffset = CGPointMake(320, 0);
-        }
-        else if(scrollView.contentOffset.x == 0)
-        {
-            scrollView.contentOffset = CGPointMake((self.headerArray.count - 2)*320,0);
-        }
-        
 
-    }else
-    {
         CGFloat offsetY = scrollView.contentOffset.y;
         UIView *headerContentView = self.tableView.tableHeaderView.subviews[0];
         headerContentView.transform = CGAffineTransformMakeTranslation(0, MIN(offsetY, 0));
-    }
 }
-- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView
-{
-    //circul scrollView
-    
-}
+
 @end
